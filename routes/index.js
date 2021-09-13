1961011ca28110cb08758be171b588b8c2ab212c
Manvi Bansal <bansalmanvi1409@gmail.com>
2021-09-13 12:41:01 -0500

    removed extra print statements

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',function(req,res){
-    console.log(req.cookies);
+   
     return res.render('home', {
         title: 'Home Page'
     });
